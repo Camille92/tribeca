355b80d6cc158bb5b3beafc316fc448c5b78ba8c
Camille92 <Camille92@users.noreply.github.com>
2017-02-02 14:17:59 +0100

    "Magical" fix

--- src/service/gateways/okcoin.ts
+++ src/service/gateways/okcoin.ts
@@ -425,7 +425,7 @@ class OkCoinPositionGateway implements Interfaces.IPositionGateway {
 
     private _log = Utils.log("tribeca:gateway:OkCoinPG");
     constructor(private _http : OkCoinHttp) {
-        setInterval(this.trigger, 7500);
+        setInterval(this.trigger, 1500);
         setTimeout(this.trigger, 10);
     }
 }
