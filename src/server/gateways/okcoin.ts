cb440e6a71a560735d98cc4bc320bc5a2598624b
Camille92 <Camille92@users.noreply.github.com>
2017-02-11 15:23:02 +0100

    Fixed Okcoin

--- src/server/gateways/okcoin.ts
+++ src/server/gateways/okcoin.ts
@@ -106,6 +106,7 @@ class OkCoinWebsocket {
                   return this._log.info("Successfully connected to %s", msg.channel);
                 if (typeof msg.errorcode !== "undefined" && (
                   msg.errorcode == '10005'
+                  || msg.errorcode == '10009'                  
                 ))  return;
             }
 
