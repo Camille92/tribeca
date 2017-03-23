eb41d8cd173f92ec9850914a520100baaf897187
Camille92 <Camille92@users.noreply.github.com>
2017-03-23 09:47:42 +0100

    Update okcoin.ts

--- src/server/gateways/okcoin.ts
+++ src/server/gateways/okcoin.ts
@@ -100,14 +100,14 @@ class OkCoinWebsocket {
                   && msg.errorcode != '10009' /* 10009=Order does not exist */
                   && msg.errorcode != '10010' /* 10010=Insufficient funds */
                   && msg.errorcode != '10016' /* 10016=Insufficient coins balance */
-                  // msg.errorcode != '10001' /* 10001=Request frequency too high */
+                  && msg.errorcode != '20103' /* 10001=Request frequency too high */
                 ))) this._log.warn("Unsuccessful message %s received.", raw);
                 else if (msg.success === "true")
                   return this._log.info("Successfully connected to %s", msg.channel);
                 if (typeof msg.errorcode !== "undefined" && (
                   msg.errorcode == '10050'
                   || msg.errorcode == '10009'
-                  // || msg.errorcode == '10001'
+                  || msg.errorcode == '20103'
                 ))  return;
             }
 
